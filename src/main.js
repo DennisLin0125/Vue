230b948b00cc7e76ce343469a0a8a675c61a1ede
Dennis <740125aa@gmail.com>
2023-10-08 16:37:06 +0800

    基本配置

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
+/**
+ * 這是入口文件
+ */
+
 import Vue from 'vue'
 import App from './App.vue'
 
+// 關閉生產提示
 Vue.config.productionTip = false
 
+// 創建Vue
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+})
