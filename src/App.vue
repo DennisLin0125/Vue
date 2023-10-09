4929318935c05e12c2101a50b1afa26492451571
Dennis <740125aa@gmail.com>
2023-10-09 23:20:34 +0800

    過度效果

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <testVm/>
+    <testVm2/>
   </div>
 </template>
 
 <script>
 import testVm from './components/testVm.vue'
+import testVm2 from './components/testVm2.vue'
 export default {
   name: 'App',
-  components: {testVm}
+  components: {testVm,testVm2}
 }
 </script>
 
---
+++ src/components/testVm2.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">顯示/隱藏</button>
+    <transition name="hello" appear>
+      <h1 v-show="isShow">你好啊</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "testVm2",
+  data() {
+    return {
+      isShow: true
+    }
+  },
+}
+</script>
+
+<style scope>
+h1 {
+  background-color: orange;
+}
+
+/* 進入的起點  離開的終點 */
+.hello-leave-to,
+.hello-enter {
+  transform: translateX(-100%);
+}
+
+/* 進入的過程中  離開的過程中*/
+.hello-enter-active,
+.hello-leave-active {
+  transition: 0.5s linear;
+}
+
+/* 進入的終點 離開的起點 */
+.hello-leave,
+.hello-enter-to {
+  transform: translateX(0);
+}
+</style>
