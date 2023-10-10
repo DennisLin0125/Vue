e037765e2a4131d49bc25a74d6c4aa46d50a753b
Dennis <740125aa@gmail.com>
2023-10-10 14:22:13 +0800

    配置代理方式1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div>
-    
+    <button @click="getStudent">獲取學生訊息</button>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'App',
-    components: {
+import axios from 'axios'
+export default {
+  name: 'App',
+  components: {
 
+  },
+  methods: {
+    getStudent() {
+      axios.defaults.baseURL = "http://localhost:8081"
+      axios({
+        method: "GET",
+        url: "/students",
+      }).then(res => {
+        console.log("請求成功:", res.data);
+      }).catch((reason) => {
+        console.error("失敗了:", reason.message);
+      })
     }
   }
+}
 </script>
 
-<style>
-
-</style>
+<style></style>
