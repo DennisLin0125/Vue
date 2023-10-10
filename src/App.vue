7a61abd739b0ad306303d0e3a11effb9a2f9ec99
Dennis <740125aa@gmail.com>
2023-10-10 20:30:20 +0800

    默認插槽

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,44 @@
 <template>
-  <div>
-    
+  <div class="container">
+    <Categroy title="美食">
+      <img src="https://s3.ax1x.com/2021/01/16/srJlq0.jpg" alt="logo" />
+    </Categroy>
+
+    <Categroy title="遊戲">
+      <ul>
+        <li v-for="(game, index) in games" :key="index">{{ game }}</li>
+      </ul>
+    </Categroy>
+
+    <Categroy title="電影"> 
+      <video controls src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"></video>
+    </Categroy>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'App',
-    components: {
-
-    }
-  }
+import Categroy from "./components/Categroy.vue";
+export default {
+  name: "App",
+  components: { Categroy },
+  data() {
+    return {
+      foods: ["火鍋", "燒烤", "小龍蝦", "牛排"],
+      games: ["紅色警戒", "穿越火線", "勁舞團", "傳說對決"],
+      films: ["《教父》", "《拆蛋專家》", "《賭神》", "《上海灘》"],
+    };
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-around;
+}
 
+img,
+video {
+  width: 100%;
+}
 </style>
---
+++ src/components/Categroy.vue
@@ -0,0 +1,28 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="categroty">
+    <h3>{{ title }}分類</h3>
+    <!-- 定義一個插槽給組件 -->
+    <slot>我是插槽默認值</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Categroy",
+  props: ["title"],
+};
+</script>
+
+<style scoped>
+.categroty {
+  background-color: skyblue;
+  width: 200px;
+  height: 300px;
+}
+h3 {
+  text-align: center;
+  background-color: orange;
+}
+</style>
