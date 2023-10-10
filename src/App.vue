d2adce08bac0a6b4ea70ddb9b8c8ab4416035b75
Dennis <740125aa@gmail.com>
2023-10-10 13:30:32 +0800

    動畫效果 利用第三方庫

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div>
     <testVm/>
     <testVm2/>
+    <testVm3/>
   </div>
 </template>
 
 <script>
 import testVm from './components/testVm.vue'
 import testVm2 from './components/testVm2.vue'
+import testVm3 from './components/testVm3.vue'
+
 export default {
   name: 'App',
-  components: {testVm,testVm2}
+  components: {testVm,testVm2,testVm3}
 }
 </script>
 
---
+++ src/components/testVm3.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">顯示/隱藏</button>
+    <transition-group
+      appear 
+      name="animate__animated animate__bounce"  
+      enter-active-class="animate__swing"
+      leave-active-class="animate__backOutUp"
+    >
+      <h1 v-show="!isShow" key="1">你好啊</h1>
+      <h1 v-show="isShow" key="2">DENNIS</h1>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+import 'animate.css'
+export default {
+  name: "testVm2",
+  data() {
+    return {
+      isShow: true
+    }
+  },
+}
+</script>
+
+<style scope>
+h1 {
+  background-color: orange;
+}
+
+</style>
