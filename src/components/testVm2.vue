f8e21f089576786911bde0d8fc47061db42bfcbe
Dennis <740125aa@gmail.com>
2023-10-10 13:10:45 +0800

    多個元素過度

--- src/components/testVm2.vue
+++ src/components/testVm2.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <button @click="isShow = !isShow">顯示/隱藏</button>
-    <transition name="hello" appear>
-      <h1 v-show="isShow">你好啊</h1>
-    </transition>
+    <transition-group name="hello" appear>
+      <h1 v-show="!isShow" key="1">你好啊</h1>
+      <h1 v-show="isShow" key="2">DENNIS</h1>
+    </transition-group>
   </div>
 </template>
 
