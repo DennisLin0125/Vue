71792741d5522260bf92c852e801bbe8f6ffa4d1
Dennis <740125aa@gmail.com>
2023-10-10 15:38:21 +0800

    github案例  靜態組件

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,43 @@
 <template>
-  <div>
-    <button @click="getStudent">獲取學生訊息</button>
-    <button @click="getCar">獲取汽車訊息</button>
+  <div class="container">
+    <userSearch />
+    <userList />
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import userList from './components/userList.vue'
+import userSearch from './components/userSearch.vue'
+
+// import axios from 'axios'
 export default {
   name: 'App',
-  components: {
-
-  },
-  methods: {
-    getStudent() {
-      axios.defaults.baseURL = "http://localhost:8081"
-      axios({
-        method: "GET",
-        url: "/students",
-      }).then(res => {
-        console.log("請求成功:", res.data);
-      }).catch((reason) => {
-        console.error("失敗了:", reason.message);
-      })
-    },
-    getCar(){
-      axios.defaults.baseURL = "http://localhost:8081"
-      axios({
-        method: "GET",
-        url: "/cars",
-      }).then(res => {
-        console.log("請求成功:", res.data);
-      }).catch((reason) => {
-        console.error("失敗了:", reason.message);
-      })
-    }
-  }
+  components: { userList, userSearch },
+  // methods: {
+  //   getStudent() {
+  //     axios.defaults.baseURL = "http://localhost:8081"
+  //     axios({
+  //       method: "GET",
+  //       url: "/students",
+  //     }).then(res => {
+  //       console.log("請求成功:", res.data);
+  //     }).catch((reason) => {
+  //       console.error("失敗了:", reason.message);
+  //     })
+  //   },
+  //   getCar() {
+  //     axios.defaults.baseURL = "http://localhost:8081"
+  //     axios({
+  //       method: "GET",
+  //       url: "/cars",
+  //     }).then(res => {
+  //       console.log("請求成功:", res.data);
+  //     }).catch((reason) => {
+  //       console.error("失敗了:", reason.message);
+  //     })
+  //   }
+  // }
 }
 </script>
 
-<style></style>
+
