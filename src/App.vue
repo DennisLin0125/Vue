bab9ec356c742eb29fbb7590e337edacd4f3bdb6
Dennis <740125aa@gmail.com>
2023-10-14 19:19:57 +0800

    router link的replace屬性

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@
       <div class="col-xs-2 col-xs-offset-2">
         <div class="list-group">
           <!-- Vue利用 router-link 標籤 實現路由切換-->
-          <router-link class="list-group-item" active-class="active" :to="{name: 'default'}">About</router-link>
-          <router-link class="list-group-item" active-class="active" to="/home">Home</router-link>
+          <router-link replace class="list-group-item" active-class="active" :to="{name: 'default'}">About</router-link>
+          <router-link replace class="list-group-item" active-class="active" to="/home">Home</router-link>
         </div>
       </div>
       <div class="col-xs-6">
