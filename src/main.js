a40a989f17ac36b279fe974724c97a13d539236a
Dennis <740125aa@gmail.com>
2023-10-15 00:16:47 +0800

    element ui按需引入

--- src/main.js
+++ src/main.js
@@ -5,6 +5,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 參考 https://element.eleme.io/#/zh-CN/component/installation
+// import ElementUI from 'element-ui';    // npm i element-ui
+// import 'element-ui/lib/theme-chalk/index.css';
+
+// Vue.use(ElementUI);
+
+// 按需引入
+import { Button, Row, DatePicker } from 'element-ui';
+
+Vue.component('el-button', Button);
+Vue.component('el-row', Row);
+Vue.component('el-date-picker', DatePicker);
+
 // 關閉生產提示
 Vue.config.productionTip = false
 
